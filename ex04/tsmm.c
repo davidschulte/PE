e537a17bb61f06c70d87ed8216b39256a8b0b025
Ein Kursaccount <pentq016@cheops0.cheops>
2018-11-15 13:18:56 +0100

    running

--- ex04/tsmm.c
+++ ex04/tsmm.c
@@ -3,19 +3,22 @@
 #include <omp.h>
 #include <sched.h>
 
+
+void printMatrix1D(double *a, char* name, int n, int m, int rows);
+
 int main()
 {
 
 int nb = 2;
-int n = 10;
+int n = 4;
 
-double M[2][2];
-double *V = (double*) malloc(size(double) * n * nb);
-double *W = (double*) malloc(size(double) * n * nb);
+double M[nb][nb];
+double *V = malloc(sizeof(double) * n * nb);
+double *W = malloc(sizeof(double) * n * nb);
 
 for(int i = 0; i < n; i++)
-	for(int j = 0; i < nb; j++)
-		V[i*n+j] = j+1;
+	for(int j = 0; j < nb; j++)
+		V[i*nb+j] = j+1;
 
 for(int i = 0; i < nb; i++)
 	for(int j = 0; j < nb; j++)
@@ -23,14 +26,45 @@ for(int i = 0; i < nb; i++)
 
 for(int i = 0; i < n; i++)
 	for(int j = 0; j < nb; j++)
-		W[i*n+j] = 0;
-
-for(int i = 0; i < n; i++)
-	for(int j = 0; j < nb; j++)
+		{
+                W[i*nb+j] = 0;
 		for(int k = 0 ; k < nb; k++)
-			W[i+n*j] = W[i+n*j] + V[i*n+k] * M[k][j];
+			W[i*nb+j] = W[i*nb+j] + V[i*nb+k] * M[k][j];
+		}
+
+printMatrix1D(V, "V", n, nb, 1);
+printMatrix1D(W, "W", n, nb, 1);
 
 
+free(V);
+free(W);
 
 return 0;
+}
+
+
+void printMatrix1D(double *a, char* name, int n, int m, int rows)
+{
+        printf("Matrix %s:\n", name);
+	if(rows == 1)
+	{
+        	for(int i = 0; i < n; i++)
+                {
+                	for(int j = 0; j < m; j++)
+                        	printf("%f\t", a[i*m+j]);
+                	printf("\n");
+                }
+        	printf("\n\n");
+	}
+	else
+	{
+                for(int i = 0; i < n; i++)
+                {
+                        for(int j = 0; j < m; j++)
+                                printf("%f\t", a[i+j*n]);
+                        printf("\n");
+                }
+                printf("\n\n");
+        }
+
 }
